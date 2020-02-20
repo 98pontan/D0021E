16f5fbedb8eee53ef0478a598f2aa7c25d479e58
98pontan <pontus.jirbratt@hotmail.com>
2020-02-20 14:59:34 +0100

    worked with moveInterface

--- Simulator/src/Sim/MoveInterface.java
+++ Simulator/src/Sim/MoveInterface.java
@@ -2,11 +2,25 @@ package Sim;
 
 public class MoveInterface implements Event {
 	
-	private NetworkAddr lastInterface;
-	private int newInterface;
+	private NetworkAddr _lastInterface;
+	private int _newInterfaceNumber;
+	
+	MoveInterface(NetworkAddr source, int newInterfaceNumber) {
+		// TODO Auto-generated method stub
+		_lastInterface = source;
+		_newInterfaceNumber = newInterfaceNumber;
+	}
+	
+	public NetworkAddr lastInterface() {
+		return _lastInterface;
+	}
+	
+	public int _newInterfaceNumber(){
+		return _newInterfaceNumber;
+	}
+	
 	
 	public void entering(SimEnt locale) {
-		// TODO Auto-generated method stub
 		
 	}
 }
--- Simulator/src/Sim/Run.java
+++ Simulator/src/Sim/Run.java
@@ -1,6 +1,7 @@
 package Sim;
 import Sim.LossyLink;
 
+
 // An example of how to build a topology and starting the simulation engine
 
 public class Run {
@@ -20,12 +21,12 @@ public class Run {
 		//Node host1 = new Node(1,1);
 		//Node host2 = new Node(2,1);
 		Sink host2 = new Sink(2, 1);
-
+		
 		// CBR
-		//Generator_CBR host1 = new Generator_CBR(1,1);
+		Generator_CBR host1 = new Generator_CBR(1,1);
 
 		//Gaussian
-		Generator_Gaussian host1 = new Generator_Gaussian(1, 1);
+		//Generator_Gaussian host1 = new Generator_Gaussian(1, 1);
 
 		//Poisson
 		//Generator_Poisson host1 = new Generator_Poisson(1, 1);
@@ -35,24 +36,25 @@ public class Run {
 		//Connect links to hosts
 		host1.setPeer(link1);
 		host2.setPeer(link2);
-
 		// Creates as router and connect
 		// links to it. Information about
 		// the host connected to the other
 		// side of the link is also provided
 		// Note. A switch is created in same way using the Switch class
 		Router routeNode = new Router(2);
+		
 		routeNode.connectInterface(0, link1, host1);
 		routeNode.connectInterface(1, link2, host2);
-
+		
 		//Poisson
 		//host1.StartSending(2, 5, 1, 10000);
 
 		//Gaussian
-		host1.StartSending(2, 2, 3, 8, 10000);
+		//host1.StartSending(2, 2, 3, 8, 10000);
 
 		//CBR
-		//host1.StartSending(1, 1, 10000, 3);
+		host1.StartSending(1, 1, 10, 3);
+		
 		// Generate some traffic
 		// host1 will send 20 messages with time interval 5 to network 2, node 1. Sequence starts with number 1
 		//host1.StartSending(2, 2, 10, 6, 1);
@@ -75,4 +77,5 @@ public class Run {
 		}
 
 	}
+
 }
