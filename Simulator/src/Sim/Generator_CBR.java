f3344518ab2c80b363ae39e839b21f3a72bfcb15
Oscar Rosberg <oscros-7@student.ltu.se>
2020-02-20 09:55:45 +0100

    -

--- Simulator/src/Sim/Generator_CBR.java
+++ Simulator/src/Sim/Generator_CBR.java
@@ -1,59 +1,51 @@
 package Sim;
 
 public class Generator_CBR extends Node {
-	private double _time = 0.0; // 
-	protected int _packets_per_second; // Number of packets per second
-	private int _stopSendingAfter = 0; // Number of packets that the genereator will generate
-	
-	
-	
-	public Generator_CBR(int network, int node) {
-		super(network, node);
-		_id = new NetworkAddr(network, node); //id of the network
-	}
+    private double _time = 0.0; //
+    protected int _packets_per_second; // Number of packets per second
+    private int _stopSendingAfter = 0; // Number of packets that the genereator will generate
 
 
-	public void StartSending(int network, int node, int number, int packets_per_second)
-	{
-		_stopSendingAfter = number;
-		_packets_per_second = packets_per_second;
-		_toNetwork = network;
-		_toHost = node;
-		_seq = 1;
-		send(this, new TimerEvent(),0);
-	}
+    public Generator_CBR(int network, int node) {
+        super(network, node);
+        _id = new NetworkAddr(network, node); //id of the network
+    }
 
-	public void recv(SimEnt src, Event ev)
-	{
-		if (ev instanceof TimerEvent)
-		{
-			if (_stopSendingAfter > _sentmsg)
-			{
-				double tempT = 1.0/_packets_per_second;
-				//int package_number = 1;
-				for(int i = 0; i < _stopSendingAfter; i++) {
-					
-				
-				System.out.println("Packge number " + _seq + "Time " + (_time + SimEngine.getTime()));
-				_sentmsg++;
-				send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost),_seq),_time);
-				send(this, new TimerEvent(), _time);
-				System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" sent message with seq: "+_seq + " at time "+SimEngine.getTime());
-				System.out.println("SimTime:" + SimEngine.getTime());
-				_seq++;
-				_time += tempT;
-				//package_number ++;
-				}
-				
-			
-			}
-			send(this, new TimerEvent(),1);
-		}
-		if (ev instanceof Message)
-		{
-			System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" receives message with seq: "+((Message) ev).seq() + " at time "+SimEngine.getTime());
-			
-		}
-	}
+
+    public void StartSending(int network, int node, int number, int packets_per_second) {
+        _stopSendingAfter = number;
+        _packets_per_second = packets_per_second;
+        _toNetwork = network;
+        _toHost = node;
+        _seq = 1;
+        send(this, new TimerEvent(), 0);
+    }
+
+    public void recv(SimEnt src, Event ev) {
+        if (ev instanceof TimerEvent) {
+            if (_stopSendingAfter > _sentmsg) {
+                double tempT = 1.0 / _packets_per_second;
+                //int package_number = 1;
+                for (int i = 0; i < _stopSendingAfter; i++) {
+					TimeWriter.logTime(SimEngine.getTime()+_time, "CBR");
+					_sentmsg++;
+                    send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost), _seq), _time);
+                    send(this, new TimerEvent(), _time);
+                    System.out.println("Node " + _id.networkId() + "." + _id.nodeId() + " sent message with seq: " + _seq + " at time " + SimEngine.getTime());
+                    System.out.println("SimTime:" + SimEngine.getTime());
+                    _seq++;
+                    _time += tempT;
+                    //package_number ++;
+                }
+
+
+            }
+            send(this, new TimerEvent(), 1);
+        }
+        if (ev instanceof Message) {
+            System.out.println("Node " + _id.networkId() + "." + _id.nodeId() + " receives message with seq: " + ((Message) ev).seq() + " at time " + SimEngine.getTime());
+
+        }
+    }
 
 }
--- Simulator/src/Sim/Generator_Poisson.java
+++ Simulator/src/Sim/Generator_Poisson.java
@@ -2,7 +2,18 @@ package Sim;
 
 import java.util.Random;
 
-public class Generator_Poisson {
+public class Generator_Poisson extends Node {
+    private double _time = 0.0;
+    private int _stopSendingAfter = 0; // Number of packets that the genereator will generate
+    private int standardDeviation;
+    private double mean;
+
+
+
+    public Generator_Poisson(int network, int node) {
+        super(network, node);
+        _id = new NetworkAddr(network, node);
+    }
 
     //D. Knuths algorithm
     private static int getPoissonRandom(double mean) {
@@ -15,5 +26,41 @@ public class Generator_Poisson {
             k++;
         } while (p > L);
         return k - 1;
+    }
+
+    public void StartSending(int network, int node, double mean, int number)
+    {
+        _stopSendingAfter = number;
+        this.mean = mean;
+        _toNetwork = network;
+        _toHost = node;
+        _seq = 1;
+        send(this, new TimerEvent(),0);
+    }
+
+    public void recv(SimEnt src, Event ev)
+    {
+        if (ev instanceof TimerEvent)
+        {
+            if (_stopSendingAfter > _sentmsg)
+            {
+                for(int i = 0; i < _stopSendingAfter; i++) {
+                    double poissonValue = getPoissonRandom(mean);
+                    TimeWriter.logTime(poissonValue, "poisson");
+                    _sentmsg++;
+                    System.out.println("Poisson value is: " + poissonValue);
+                    send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost),_seq), poissonValue);
+                    send(this, new TimerEvent(), poissonValue);
+                    System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" sent message with seq: "+_seq + " at time "+(SimEngine.getTime()+poissonValue));
+                    _seq++;
+                }
+            }
+            send(this, new TimerEvent(),1);
+        }
+        if (ev instanceof Message)
+        {
+            System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" receives message with seq: "+((Message) ev).seq() + " at time "+SimEngine.getTime());
+
+        }
     }
 }
