2589c5c43eed7e4273d27f6f5da0e0eee23aefa2
98pontan <pontus.jirbratt@hotmail.com>
2020-02-28 10:23:03 +0100

    working on HA

--- Simulator/src/Sim/HomeAgent.java
+++ Simulator/src/Sim/HomeAgent.java
@@ -1,30 +1,36 @@
 package Sim;
 
 public class HomeAgent extends ChangeableRouterInterface {
-
+	protected HomeAgentEntry _homeAgentTable;
+	private NetworkAddr address;
 	HomeAgent(int interfaces) {
 		super(interfaces);
 		// TODO Auto-generated constructor stub
-		protected HomeAgentEntry currentLocation;
-		protected HomeAgentEntry  homeLocation;
-		
-		
 	}
 	
+	public void recv(SimEnt source, Event event) {
+        System.out.println("Event Type rcv in router: " + event);
+        System.out.println("Src: " + source);
+        if(event instanceof NotifyHAEvent) {
+        	// call function that changes network address
+        }
+        
+        if (event instanceof MoveInterfaceEvent) {
+            changeInterface(((MoveInterfaceEvent) event)._oldInterface, ((MoveInterfaceEvent) event)._newInterfaceNumber());
+          
+        }
+        if (event instanceof Message) {
+            System.out.println("Router handles packet with seq: " + ((Message) event).seq() + " from node: " + ((Message) event).source().networkId() + "." + ((Message) event).source().nodeId());
+            SimEnt sendNext = getInterface(((Message) event).destination().networkId());
+            System.out.println("Router sends to node: " + ((Message) event).destination().networkId() + "." + ((Message) event).destination().nodeId());
+            send(sendNext, event, _now);
+        }
+ 
 	
-	
-	public void Handover(int newInterfaceNumber, int oldInterfaceNumber) {
-		if (currentLocation == homeLocation) {
-			_routingTable[currentLocation] = route;
-			 System.out.println("You are still att Home");
-	
-		}
-		else {
-            RouteTableEntry route = _routingTable[oldInterfaceNumber];
-            _routingTable[oldInterfaceNumber] = null;
-            _routingTable[newInterfaceNumber] = route;
-		}
-
-	}
+        
+     public void networkChanger(NetworkAddr address, ) {
+    	 
+    }
+}
 	
 }
