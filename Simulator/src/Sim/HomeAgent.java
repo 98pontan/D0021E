1d51d316523e34f3fb4185adf15fb6e2fd6f5fd4
98pontan <pontus.jirbratt@hotmail.com>
2020-03-11 16:54:40 +0100

    added some documentation

--- Simulator/src/Sim/HomeAgent.java
+++ Simulator/src/Sim/HomeAgent.java
@@ -3,21 +3,24 @@ package Sim;
 import java.util.HashMap;
 
 public class HomeAgent extends RouterInterfaceChanger {
-    protected HomeAgentEntry _homeAgentTable;
-    private NetworkAddr address;
-    HashMap<NetworkAddr, NetworkAddr> routingTable;
+    protected HomeAgentEntry _homeAgentTable; // a table we will most likely not use
+    private NetworkAddr address; // for holding the new Address for lookups in the HashMap
+    HashMap<NetworkAddr, NetworkAddr> routingTable; // HashMap for address mapping
 
+    // Initiates HashMap
     HomeAgent(int interfaces) {
         super(interfaces);
         this.routingTable = new HashMap<>();
         // TODO Auto-generated constructor stub
     }
 
+    // function for receiving events
     public void recv(SimEnt source, Event event) {
         System.out.println("Event Type rcv in router: " + event);
         System.out.println("Src: " + source);
         if (event instanceof NotifyHAEvent) {
-            // call function that changes network address
+        	
+            // call function that maps the new address to the homeaddress  also calling the wanted data
             System.out.println(((NotifyHAEvent) event).getHomeAddress());
             networkChanger(((NotifyHAEvent) event).getHomeAddress(), ((NotifyHAEvent) event).getCareOfAddress());
         }
@@ -26,6 +29,8 @@ public class HomeAgent extends RouterInterfaceChanger {
             changeInterface(((MoveInterfaceEvent) event)._oldInterface(), ((MoveInterfaceEvent) event)._newInterfaceNumber());
 
         }
+        
+        
         if (event instanceof Message) {
             System.out.println("Router handles packet with seq: " + ((Message) event).seq() + " from node: " + ((Message) event).source().networkId() + "." + ((Message) event).source().nodeId());
             SimEnt sendNext = getInterface(((Message) event).destination().networkId());
@@ -36,7 +41,7 @@ public class HomeAgent extends RouterInterfaceChanger {
             send(sendNext, event, _now);
         }
     }
-
+    // Maps homeaddress to the new address
     public void networkChanger(NetworkAddr homeAddress, NetworkAddr careOfAddress){
         routingTable.put(homeAddress, careOfAddress);
         address = careOfAddress;
--- Simulator/src/Sim/NotifyHAEvent.java
+++ Simulator/src/Sim/NotifyHAEvent.java
@@ -1,5 +1,5 @@
 package Sim;
-
+// A class for creating an event when a node has changed position. 
 public class NotifyHAEvent implements Event{
     private NetworkAddr careOfAddress;
     private NetworkAddr homeAddress;
