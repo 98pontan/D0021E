e600445a9de2aacc1bf925ec2e670ce38f20112c
Pontus Jirbratt <pontus.jirbratt@hotmail.com>
2020-03-13 13:58:42 +0100

    created a boolean flag in HomeAgent and added a new class for ACK

--- Simulator/src/Sim/HomeAgent.java
+++ Simulator/src/Sim/HomeAgent.java
@@ -3,19 +3,31 @@ package Sim;
 import java.util.HashMap;
 
 public class HomeAgent extends RouterInterfaceChanger {
-    protected HomeAgentEntry _homeAgentTable;
+    protected HomeAgentEntry _homeAgentTable; 
     private NetworkAddr address;
-    HashMap<NetworkAddr, NetworkAddr> routingTable;
+    protected HashMap<NetworkAddr, NetworkAddr> routingTable; // HashMap for mapping home addresses to the new address, Key HomeAddress Value foreign address 
 
     HomeAgent(int interfaces) {
         super(interfaces);
-        this.routingTable = new HashMap<>();
+        this.routingTable = new HashMap<>(); // Generating HashMaps
         // TODO Auto-generated constructor stub
     }
+    
+    public boolean flag(int nodeId) {
+    	for(NetworkAddr i: routingTable.keySet()) {
+    		if (i.nodeId() == nodeId) {
+    			return true;
+    		}
+    		
+    	}
+		return false;    	
+    }
 
+    
     public void recv(SimEnt source, Event event) {
         System.out.println("Event Type rcv in router: " + event);
         System.out.println("Src: " + source);
+        
         if (event instanceof NotifyHAEvent) {
             // call function that changes network address
             System.out.println(((NotifyHAEvent) event).getHomeAddress());
@@ -29,6 +41,7 @@ public class HomeAgent extends RouterInterfaceChanger {
         if (event instanceof Message) {
             System.out.println("Router handles packet with seq: " + ((Message) event).seq() + " from node: " + ((Message) event).source().networkId() + "." + ((Message) event).source().nodeId());
             SimEnt sendNext = getInterface(((Message) event).destination().networkId());
+ //          SimEnt sendNext = getInterface((Message) event)
             System.out.println("Router sends to node: " + ((Message) event).destination().networkId() + "." + ((Message) event).destination().nodeId());
             send(sendNext, event, _now);
         }
@@ -36,7 +49,7 @@ public class HomeAgent extends RouterInterfaceChanger {
 
     public void networkChanger(NetworkAddr homeAddress, NetworkAddr careOfAddress){
         routingTable.put(homeAddress, careOfAddress);
-        System.out.println("fan");
+        System.out.println("Test for networkChanger");
         System.out.println(routingTable);
 
     }
